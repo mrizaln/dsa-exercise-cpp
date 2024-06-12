136579938b8817a57efd7ed7b5568c0096dd5c0e
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-06-12 23:27:51 +0700

    Add stack implementation

---
+++ include/dsa/stack.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <concepts>
+#include <utility>
+
+namespace dsa
+{
+    template <template <typename> typename C, typename T>
+    concept StackCompatible = requires(C<T> c, const C<T> cc, T&& t) {
+        { cc.back() } -> std::same_as<const T&>;
+        { c.back() } -> std::same_as<T&>;
+        { c.push_back(std::move(t)) } -> std::same_as<T&>;
+        { c.pop_back() } -> std::same_as<T>;
+        { c.size() } -> std::same_as<std::size_t>;
+    };
+
+    template <template <typename> typename C, typename T>
+        requires StackCompatible<C, T>
+    class Stack
+    {
+    public:
+        using Container = C<T>;
+        using Element   = T;
+
+        template <typename... Args>
+            requires std::constructible_from<Container, Args...>
+        Stack(Args&&... args)
+            : m_container(std::forward<Args>(args)...)
+        {
+        }
+
+        bool        empty() const { return m_container.size() == 0; }
+        std::size_t size() const { return m_container.size(); }
+        T&          push(T&& t) { return m_container.push_back(std::move(t)); }
+        T           pop() { return m_container.pop_back(); }
+        T&          top() { return m_container.back(); }
+        const T&    top() const { return m_container.back(); }
+
+    private:
+        Container m_container;
+    };
+}
---
+++ test/stack.cpp
@@ -0,0 +1,93 @@
+#include <dsa/stack.hpp>
+#include <dsa/list.hpp>
+
+#include <boost/ut.hpp>
+#include <boost/ut.hpp>
+#include <fmt/core.h>
+#include <fmt/ranges.h>
+#include <fmt/std.h>
+
+#include <ranges>
+#include <concepts>
+
+namespace ut = boost::ut;
+namespace rr = std::ranges;
+namespace rv = rr::views;
+
+class NonTrivial
+{
+public:
+    // clang-format off
+    NonTrivial() = default;
+    NonTrivial(int value) : m_value(value) { }
+
+    NonTrivial(const NonTrivial& other)     : m_value(other.m_value) { }
+    NonTrivial(NonTrivial&& other) noexcept : m_value(other.m_value) { }
+
+    NonTrivial& operator=(const NonTrivial& other)     { m_value = other.m_value; return *this; }
+    NonTrivial& operator=(NonTrivial&& other) noexcept { m_value = other.m_value; return *this; }
+    int value() const { return m_value; }
+    // clang-format on
+
+    friend std::ostream& operator<<(std::ostream& os, const NonTrivial& nt) { return os << nt.value(); }
+    friend int           format_as(const NonTrivial& nt) { return nt.value(); }
+
+    auto operator<=>(const NonTrivial&) const = default;
+
+private:
+    int m_value = 0;
+};
+
+static_assert(std::movable<NonTrivial> and std::copyable<NonTrivial>);
+
+int main()
+{
+    using namespace ut::operators;
+    using namespace ut::literals;
+    using ut::expect, ut::that, ut::throws, ut::nothrow;
+
+    "ArrayList, LinkedList, and DoublyLinkedList should be able to be used as Stack backend"_test = [] {
+        static_assert(dsa::StackCompatible<dsa::ArrayList, NonTrivial>);
+        static_assert(dsa::StackCompatible<dsa::LinkedList, NonTrivial>);
+        static_assert(dsa::StackCompatible<dsa::DoublyLinkedList, NonTrivial>);
+    };
+
+    "Stack should be able to be constructed with a backend"_test = [] {
+        dsa::Stack<dsa::ArrayList, NonTrivial>        stack{ 10 };    // limited capacity
+        dsa::Stack<dsa::LinkedList, NonTrivial>       stack2;         // shouldn't be used
+        dsa::Stack<dsa::DoublyLinkedList, NonTrivial> stack3;
+    };
+
+    "Stack with ArrayList backend should be able to push and pop"_test = [] {
+        dsa::Stack<dsa::ArrayList, NonTrivial> stack{ 10 };
+
+        for (auto i : rv::iota(0, 10)) {
+            stack.push(i);
+        }
+
+        expect(stack.size() == 10_i);
+        expect(throws([&] { stack.push(10); }));
+
+        for (auto i : rv::iota(0, 10) | rv::reverse) {
+            expect(that % stack.top() == i);    // just a view
+            expect(that % stack.pop() == i);
+        }
+        expect(stack.empty()) << "stack should be empty after popping all elements";
+    };
+
+    "Stack with LinkedList backend should be able to push and pop"_test = [] {
+        dsa::Stack<dsa::LinkedList, NonTrivial> stack;
+
+        for (auto i : rv::iota(0, 10)) {
+            stack.push(i);
+        }
+
+        expect(stack.size() == 10_i);
+
+        for (auto i : rv::iota(0, 10) | rv::reverse) {
+            expect(that % stack.top() == i);    // just a view
+            expect(that % stack.pop() == i);
+        }
+        expect(stack.empty()) << "stack should be empty after popping all elements";
+    };
+}
