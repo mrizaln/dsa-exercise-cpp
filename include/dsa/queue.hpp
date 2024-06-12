e650cc226d5cf3c9a6559f9d2979e9b691aba545
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-06-12 23:52:24 +0700

    Add queue implementation

---
+++ include/dsa/queue.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <concepts>
+#include <utility>
+
+namespace dsa
+{
+    template <template <typename> typename C, typename T>
+    concept QueueCompatible = requires(C<T> c, const C<T> cc, T&& t) {
+        { cc.back() } -> std::same_as<const T&>;
+        { cc.front() } -> std::same_as<const T&>;
+        { cc.size() } -> std::same_as<std::size_t>;
+        { c.back() } -> std::same_as<T&>;
+        { c.front() } -> std::same_as<T&>;
+        { c.push_back(std::move(t)) } -> std::same_as<T&>;
+        { c.pop_front() } -> std::same_as<T>;
+    };
+
+    template <template <typename> typename C, typename T>
+        requires QueueCompatible<C, T>
+    class Queue
+    {
+    public:
+        using Container = C<T>;
+        using Element   = T;
+
+        template <typename... Args>
+            requires std::constructible_from<Container, Args...>
+        Queue(Args&&... args)
+            : m_container(std::forward<Args>(args)...)
+        {
+        }
+
+        bool        empty() const { return m_container.size() == 0; }
+        std::size_t size() const { return m_container.size(); }
+        T&          push(T&& t) { return m_container.push_back(std::move(t)); }
+        T           pop() { return m_container.pop_front(); }
+        T&          front() { return m_container.front(); }
+        const T&    front() const { return m_container.front(); }
+        T&          back() { return m_container.back(); }
+        const T&    back() const { return m_container.back(); }
+
+    private:
+        Container m_container;
+    };
+}
---
+++ test/queue.cpp
@@ -0,0 +1,95 @@
+#include <dsa/queue.hpp>
+#include <dsa/list.hpp>
+
+#include <boost/ut.hpp>
+#include <fmt/core.h>
+
+#include <ranges>
+#include <concepts>
+
+namespace ut = boost::ut;
+namespace rr = std::ranges;
+namespace rv = rr::views;
+
+class NonTrivial
+{
+public:
+    // clang-format off
+    NonTrivial() = default;
+    NonTrivial(int value) : m_value(value) { }
+
+    NonTrivial(const NonTrivial& other)     : m_value(other.m_value) { }
+    NonTrivial(NonTrivial&& other) noexcept : m_value(other.m_value) { }
+
+    NonTrivial& operator=(const NonTrivial& other)     { m_value = other.m_value; return *this; }
+    NonTrivial& operator=(NonTrivial&& other) noexcept { m_value = other.m_value; return *this; }
+    int value() const { return m_value; }
+    // clang-format on
+
+    friend std::ostream& operator<<(std::ostream& os, const NonTrivial& nt) { return os << nt.value(); }
+    friend int           format_as(const NonTrivial& nt) { return nt.value(); }
+
+    auto operator<=>(const NonTrivial&) const = default;
+
+private:
+    int m_value = 0;
+};
+
+static_assert(std::movable<NonTrivial> and std::copyable<NonTrivial>);
+
+int main()
+{
+    using namespace ut::operators;
+    using namespace ut::literals;
+    using ut::expect, ut::that, ut::throws, ut::nothrow;
+
+    "LinkedList and DoublyLinkedList should be able to be used as Queue backend"_test = [] {
+        static_assert(dsa::QueueCompatible<dsa::LinkedList, NonTrivial>);
+        static_assert(dsa::QueueCompatible<dsa::DoublyLinkedList, NonTrivial>);
+    };
+
+    "ArrayList should not be able to be used as Queue backend"_test = [] {
+        static_assert(not dsa::QueueCompatible<dsa::ArrayList, NonTrivial>);
+    };
+
+    "Queue should be able to be constructed with a backend"_test = [] {
+        dsa::Queue<dsa::LinkedList, NonTrivial>       queue;
+        dsa::Queue<dsa::DoublyLinkedList, NonTrivial> queue2;
+    };
+
+    "Queue with LinkedList backend should be able to push and pop"_test = [] {
+        dsa::Queue<dsa::LinkedList, NonTrivial> queue;
+
+        for (auto i : rv::iota(0, 10)) {
+            auto v = queue.push(i);
+            expect(that % v == i);
+            expect(that % queue.back() == i);
+        }
+
+        expect(queue.size() == 10_i);
+
+        for (auto i : rv::iota(0, 10)) {
+            expect(that % queue.front() == i);
+            expect(that % queue.pop() == i);
+        }
+        expect(queue.empty());
+    };
+
+    "Queue with DoublyLinkedList backend should be able to push and pop"_test = [] {
+        dsa::Queue<dsa::DoublyLinkedList, NonTrivial> queue;
+
+        for (auto i : rv::iota(0, 10)) {
+            auto v = queue.push(i);
+            expect(that % v == i);
+            expect(that % queue.back() == i);
+        }
+
+        expect(queue.size() == 10_i);
+
+        for (auto i : rv::iota(0, 10)) {
+            expect(that % queue.front() == i);
+            expect(that % queue.pop() == i);
+        }
+        expect(queue.empty());
+    };
+}
