e683c335dd1135f6a7e03d3469f23cf8b059513e
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-06-20 01:31:09 +0700

    Add RootishArray implementation

---
+++ include/dsa/rootish_array.hpp
@@ -0,0 +1,339 @@
+#pragma once
+
+// NOTE: RootishArray implementation based on reference 1 and reference 2 (ArrayQueue)
+
+#include "dsa/array_list.hpp"
+
+#include <cmath>
+#include <concepts>
+#include <iterator>
+#include <stdexcept>
+
+namespace dsa
+{
+    template <typename T>
+    concept RootishArrayElement = std::movable<T> or std::copyable<T>;
+
+    // NOTE: the class has 3 invariants:
+    //  1. there is at most 1 empty block at the end. if there are more than 1, then the array will be shrunk.
+    //  2. special case when the array is empty, then there is no block not even an empty one.
+    //  3. the first push_back() will create 2 blocks; thus the 1st invariant is satisfied.
+    //  4. blocks have a size of 1, 2, 3, 4, 5, ..., b.
+    template <RootishArrayElement T>
+    class RootishArray
+    {
+    public:
+        template <bool IsConst>
+        class [[nodiscard]] Iterator;    // bidirectional iterator
+
+        friend class Iterator<false>;
+        friend class Iterator<true>;
+
+        template <bool IsConst>
+        using ReverseIterator = std::reverse_iterator<Iterator<IsConst>>;
+
+        using Element    = T;
+        using value_type = Element;    // STL compliance
+
+        using Block = ArrayList<T>;
+
+        RootishArray()  = default;
+        ~RootishArray() = default;
+
+        RootishArray(RootishArray&&) noexcept            = default;
+        RootishArray& operator=(RootishArray&&) noexcept = default;
+
+        RootishArray(const RootishArray&)            = default;
+        RootishArray& operator=(const RootishArray&) = default;
+
+        void swap(RootishArray& other) noexcept;
+        void clear() noexcept;
+
+        T& insert(std::size_t pos, T&& element);
+        T  remove(std::size_t pos);
+
+        T& push_back(T&& value) { return insert(size(), std::move(value)); }
+        T  pop_back() { return remove(size() - 1); }
+
+        auto&& at(this auto&& self, std::size_t pos);
+        auto&& block(this auto&& self, std::size_t pos) { return self.m_blocks.at(pos); }
+
+        auto&& front(this auto&& self) { return self.at(0); }
+        auto&& back(this auto&& self) { return self.at(self.size() - 1); }
+
+        std::size_t size() const noexcept;
+        const auto& blocks() const noexcept { return m_blocks; }
+
+        Iterator<false> begin() { return { this, 0 }; }
+        Iterator<false> end() { return { this, npos }; }
+        Iterator<true>  begin() const { return { this, 0 }; }
+        Iterator<true>  end() const { return { this, npos }; }
+        Iterator<true>  cbegin() const { return { this, 0 }; }
+        Iterator<true>  cend() const { return { this, npos }; }
+
+        // reverse iterator
+        ReverseIterator<false> rbegin() { return { this, 0 }; }
+        ReverseIterator<false> rend() { return { this, npos }; }
+        ReverseIterator<true>  rbegin() const { return { this, 0 }; }
+        ReverseIterator<true>  rend() const { return { this, npos }; }
+        ReverseIterator<true>  crbegin() const { return { this, 0 }; }
+        ReverseIterator<true>  crend() const { return { this, npos }; }
+
+    private:
+        struct ElementIndex
+        {
+            std::size_t block = 0;
+            std::size_t index = 0;    // local to the block
+        };
+
+        static constexpr std::size_t npos = std::numeric_limits<std::size_t>::max();
+
+        ArrayList<Block> m_blocks = {};
+
+        ElementIndex elementIndex(std::size_t pos) const noexcept;
+        auto&&       nearEmpty(this auto&& self) { return self.block(self.m_blocks.size() - 2); }
+
+        void grow();
+        void shrink();
+    };
+}
+
+// -----------------------------------------------------------------------------
+// implementation detail
+// -----------------------------------------------------------------------------
+
+namespace dsa
+{
+    template <RootishArrayElement T>
+    void RootishArray<T>::swap(RootishArray& other) noexcept
+    {
+        m_blocks.swap(other.m_blocks);
+    }
+
+    template <RootishArrayElement T>
+    void RootishArray<T>::clear() noexcept
+    {
+        m_blocks.clear();
+    }
+
+    template <RootishArrayElement T>
+    T& RootishArray<T>::insert(std::size_t pos, T&& value)
+    {
+        if (pos > size()) {
+            throw std::out_of_range{
+                std::format("Cannot insert element at position greater than size ({} > {})", pos, size())
+            };
+        }
+
+        // NOTE: 3rd invariant
+        if (size() == 0) {
+            grow();
+            grow();
+
+            return block(0).push_back(std::move(value));
+        }
+
+        if (nearEmpty().size() == nearEmpty().capacity()) {
+            grow();
+        }
+
+        auto [blockIdx, localIdx] = elementIndex(pos);
+
+        for (auto i = m_blocks.size() - 2; i > blockIdx; --i) {
+            Block& current = block(i);
+            Block& prev    = block(i - 1);
+
+            current.insert(0, prev.pop_back());
+        }
+
+        return block(blockIdx).insert(localIdx, std::move(value));
+    }
+
+    template <RootishArrayElement T>
+    T RootishArray<T>::remove(std::size_t pos)
+    {
+        if (pos >= size()) {
+            throw std::out_of_range{ "Cannot remove element at position greater than or equal to size" };
+        }
+
+        auto [blockIdx, localIdx] = elementIndex(pos);
+        auto value                = block(blockIdx).remove(localIdx);
+
+        for (auto i = blockIdx; i < m_blocks.size() - 2; ++i) {
+            Block& current = block(i);
+            Block& next    = block(i + 1);
+
+            current.push_back(next.remove(0));
+        }
+
+        // NOTE: 3rd invariant means the minimum block size is 2 when not empty
+        if (nearEmpty().size() == 0 and m_blocks.size() > 2) {
+            shrink();
+        }
+
+        return value;
+    }
+
+    template <RootishArrayElement T>
+    auto&& RootishArray<T>::at(this auto&& self, std::size_t pos)
+    {
+        auto [block, local] = self.elementIndex(pos);
+        return self.block(block).at(local);
+    }
+
+    template <RootishArrayElement T>
+    std::size_t RootishArray<T>::size() const noexcept
+    {
+        if (m_blocks.size() == 0) {
+            return 0;
+        }
+
+        auto numBlocks     = m_blocks.size() - 1;
+        auto lastBlockSize = nearEmpty().size();
+
+        return (numBlocks * (numBlocks + 1) / 2) - (numBlocks - lastBlockSize);
+    }
+
+    // NOTE: see reference 2
+    template <RootishArrayElement T>
+    RootishArray<T>::ElementIndex RootishArray<T>::elementIndex(std::size_t pos) const noexcept
+    {
+        auto blockApprox = (-3 + std::sqrt(9 + 8 * pos)) / 2;
+        auto block       = static_cast<std::size_t>(std::ceil(blockApprox));
+
+        auto local = pos - block * (block + 1) / 2;
+
+        return { block, local };
+    }
+
+    template <RootishArrayElement T>
+    void RootishArray<T>::grow()
+    {
+        auto& block = m_blocks.push_back({});
+        block.reserve(m_blocks.size());
+    }
+
+    template <RootishArrayElement T>
+    void RootishArray<T>::shrink()
+    {
+        auto capacity = m_blocks.size();
+        auto block    = m_blocks.pop_back();
+
+        assert(block.capacity() == capacity && "Block capacity must be equal to the number of blocks");
+        assert(block.size() == 0 && "Discarded block must be empty");
+        assert(m_blocks.back().size() == 0 && "Last block must be empty");
+    }
+
+    template <RootishArrayElement T>
+    template <bool IsConst>
+    class RootishArray<T>::Iterator
+    {
+    public:
+        // STL compatibility
+        using iterator_category = std::random_access_iterator_tag;
+        using value_type        = typename RootishArray::Element;
+        using difference_type   = std::ptrdiff_t;
+        using pointer           = std::conditional_t<IsConst, const value_type*, value_type*>;
+        using reference         = std::conditional_t<IsConst, const value_type&, value_type&>;
+
+        using ArrayPtr = std::conditional_t<IsConst, const RootishArray*, RootishArray*>;
+
+        Iterator() noexcept                      = default;
+        Iterator(const Iterator&)                = default;
+        Iterator& operator=(const Iterator&)     = default;
+        Iterator(Iterator&&) noexcept            = default;
+        Iterator& operator=(Iterator&&) noexcept = default;
+
+        Iterator(ArrayPtr array, std::size_t pos) noexcept
+            : m_array{ array }
+            , m_pos{ pos }
+            , m_size{ array->size() }
+        {
+        }
+
+        // for const iterator construction from iterator
+        Iterator(Iterator<false>& other)
+            : m_array{ other.m_array }
+            , m_pos{ other.m_pos }
+            , m_size{ other.m_size }
+        {
+        }
+
+        // just a pointer comparison
+        auto operator<=>(const Iterator&) const = default;
+
+        Iterator& operator+=(difference_type n)
+        {
+            m_pos += n;
+
+            // detect wrap-around then set to sentinel value
+            if (m_pos >= m_size) {
+                m_pos = RootishArray::npos;
+            }
+
+            return *this;
+        }
+
+        Iterator& operator-=(difference_type n)
+        {
+            // to allow reverse iteration
+            if (m_pos == RootishArray::npos) {
+                m_pos = m_size;
+            }
+
+            return *this += -n;
+        }
+
+        Iterator& operator++() { return (*this) += 1; }
+        Iterator& operator--() { return (*this) -= 1; }
+
+        Iterator operator++(int)
+        {
+            auto copy = *this;
+            ++(*this);
+            return copy;
+        }
+
+        Iterator operator--(int)
+        {
+            auto copy = *this;
+            --(*this);
+            return copy;
+        }
+
+        reference operator*() const
+        {
+            if (m_array == nullptr || m_pos == RootishArray::npos) {
+                throw std::out_of_range{ "Iterator is out of range" };
+            }
+            return m_array->at(m_pos);
+        }
+
+        pointer operator->() const
+        {
+            if (m_array == nullptr || m_pos == RootishArray::npos) {
+                throw std::out_of_range{ "Iterator is out of range" };
+            }
+            return &m_array->at(m_pos);
+        }
+
+        reference operator[](difference_type n) const { return *(*this + n); }
+
+        friend Iterator operator+(const Iterator& lhs, difference_type n) { return auto{ lhs } += n; }
+        friend Iterator operator+(difference_type n, const Iterator& rhs) { return rhs + n; }
+        friend Iterator operator-(const Iterator& lhs, difference_type n) { return auto{ lhs } -= n; }
+
+        friend difference_type operator-(const Iterator& lhs, const Iterator& rhs)
+        {
+            auto lpos = lhs.m_pos == RootishArray::npos ? lhs.m_size : lhs.m_pos;
+            auto rpos = rhs.m_pos == RootishArray::npos ? rhs.m_size : rhs.m_pos;
+
+            return static_cast<difference_type>(lpos) - static_cast<difference_type>(rpos);
+        }
+
+    private:
+        ArrayPtr    m_array = nullptr;
+        std::size_t m_pos   = RootishArray::npos;
+        std::size_t m_size  = 0;
+    };
+}
---
+++ test/rootish_array.cpp
@@ -0,0 +1,260 @@
+#include "test_util.hpp"
+
+#include <dsa/rootish_array.hpp>
+
+#include <boost/ut.hpp>
+
+#include <fmt/core.h>
+#include <ranges>
+
+namespace ut = boost::ut;
+namespace rr = std::ranges;
+namespace rv = rr::views;
+
+using test_util::equalUnderlying;
+using test_util::populateContainer;
+
+// TODO: check whether copy happens on operations that should not copy (unless type is not movable, then copy
+// should happen)
+template <test_util::TestClass Type>
+void test()
+{
+    using namespace ut::operators;
+    using namespace ut::literals;
+    using ut::expect, ut::that, ut::throws, ut::nothrow;
+
+    "iterator should be a random access iterator"_test = [] {
+        using Iter = dsa::RootishArray<Type>::template Iterator<false>;
+        static_assert(std::random_access_iterator<Iter>);
+
+        using ConstIter = dsa::RootishArray<Type>::template Iterator<true>;
+        static_assert(std::random_access_iterator<ConstIter>);
+    };
+
+    "array_list with 0 capacity is usable"_test = [] {
+        dsa::RootishArray<int> list{};
+        expect(list.size() == 0_i);
+
+        expect(nothrow([&] { list.push_back(42); })) << "pushing to an empty list should grow the capacity";
+        expect(that % list.size() == 1_i) << fmt::format("list: {}", list);
+
+        expect(nothrow([&] { list.pop_back(); })) << "pop from a list with 1 element";
+    };
+
+    "push_back should add an element to the end of the list"_test = [] {
+        dsa::RootishArray<Type> list{};
+
+        // first push
+        auto size = list.size();
+        list.push_back(42);
+        expect(that % list.size() == size + 1);
+        expect(list.back().value() == 42_i);
+        expect(list.front().value() == 42_i);
+
+        // push until half of the capacity
+        for (auto i : rv::iota(0, 9)) {
+            list.push_back(i);
+        }
+        expect(list.size() == 10_i);
+        expect(list.back().value() == 8_i);
+        expect(list.front().value() == 42_i);
+
+        std::array values = { 42, 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        equalUnderlying<Type>(list, values);
+
+        // push until full
+        while (list.size() < 20) {
+            list.push_back(42);
+        }
+        expect(list.size() == 20_i);
+        for (auto& value : list | rv::reverse | rv::take(10)) {
+            expect(value.value() == 42_i);
+        }
+
+        list.clear();
+
+        // back-inserter
+        rr::fill_n(std::back_inserter(list), 10, 42);
+        for (const auto& value : list) {
+            expect(value.value() == 42_i);
+        }
+    };
+
+    "pop_back should remove the last element from the list"_test = [] {
+        std::vector<int>        values = { 42, 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        dsa::RootishArray<Type> list{};
+        for (auto value : values) {
+            list.push_back(std::move(value));
+        }
+        expect(that % list.size() == values.size());
+
+        // first pop
+        auto size  = list.size();
+        auto value = list.pop_back();
+        expect(that % list.size() == size - 1);
+        expect(value.value() == 8_i);
+
+        for (auto i : rv::iota(0, 8) | rv::reverse) {
+            expect(list.pop_back().value() == i);
+        }
+        expect(list.size() == 1_i);
+        expect(list.pop_back().value() == 42_i);
+        expect(list.size() == 0_i);
+
+        expect(throws([&] { list.pop_back(); })) << "should throw when pop from empty list";
+    };
+
+    "insert should inserts value at specified position"_test = [] {
+        std::vector<int>        values = { 42, 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        dsa::RootishArray<Type> list{};
+
+        // first insert
+        list.insert(0, 42);
+
+        expect(list.size() == 1_i);
+        expect(list.front().value() == 42_i);
+        expect(list.back().value() == 42_i);
+
+        expect(throws([&] { list.insert(2, -1); })) << "out of bound insert should throws";
+
+        for (auto i : rv::iota(0, 9)) {
+            list.insert(list.size(), i);
+        }
+        expect(list.size() == 10_i);
+        expect(equalUnderlying<Type>(list, values));
+
+        // insertion in the middle
+        list.pop_back();    // to reserve one space
+        list.insert(5, -1);
+
+        expect(equalUnderlying<Type>(list | rv::take(4), values | rv::take(4)));
+        expect(list.at(5).value() == -1_i);
+        expect(equalUnderlying<Type>(list | rv::drop(6) | rv::take(4), values | rv::drop(5) | rv::take(4)));
+    };
+
+    "remove should removes value at specified position"_test = [] {
+        std::vector<int>        values = { 42, 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        dsa::RootishArray<Type> list{};
+        for (auto value : values) {
+            list.push_back(std::move(value));
+        }
+        expect(that % list.size() == values.size());
+
+        auto value = list.remove(5);
+        expect(that % value.value() == values[5]);
+        expect(list.size() == 9_i);
+        expect(equalUnderlying<Type>(list | rv::take(5), values | rv::take(5)));
+        expect(equalUnderlying<Type>(list | rv::drop(5) | rv::take(4), values | rv::drop(6) | rv::take(4)));
+
+        list.clear();
+        list.push_back(42);
+
+        expect(throws([&] { list.remove(1); })) << "out of bound removal should throws";
+        expect(list.remove(0).value() == 42_i);
+        expect(list.size() == 0_i);
+
+        expect(throws([&] { list.remove(0); })) << "removing element of an empty list should throw";
+    };
+
+    "move should leave list into an empty state that is usable"_test = [] {
+        dsa::RootishArray<Type> list{};
+        populateContainer(list, rv::iota(0, 10));
+
+        expect(list.size() == 10_i);
+        expect(equalUnderlying<Type>(list, rv::iota(0, 10)));
+
+        auto list2 = std::move(list);
+        expect(list.size() == 0_i);
+
+        expect(nothrow([&] { list.push_back(42); })) << "should throw when push to empty list";
+        expect(that % list.size() == 1_i);
+
+        expect(nothrow([&] { list.pop_back(); })) << "pop from a list with 1 element";
+    };
+
+    if constexpr (std::copyable<Type>) {
+        "copy should copy each element exactly"_test = [] {
+            dsa::RootishArray<Type> list{};
+            populateContainer(list, rv::iota(0, 10));
+
+            expect(list.size() == 10_i);
+            expect(equalUnderlying<Type>(list, rv::iota(0, 10)));
+
+            auto list2 = list;
+            expect(list2.size() == 10_i);
+            expect(rr::equal(list2, list));
+
+            auto list3 = list2;
+            expect(list3.size() == 10_i);
+            expect(rr::equal(list3, list));
+        };
+    }
+
+    // TODO: add parapeterized constructor to RootishArray
+    // if constexpr (std::default_initializable<Type>) {
+    //     "non-default constructed array_list should default initialize exactly N elements"_test = [] {
+    //         dsa::RootishArray<Type> list{ 41 };
+    //         expect(list.size() == 41_i);
+    //         expect(rr::all_of(list, [](const auto& value) { return value.stat().defaulted(); }));
+    //     };
+    // }
+
+    "the list should be correct after operated on"_test = [] {
+        constexpr std::array operations{ 987, 567, 417, 765, 12, 1, 75, 112 };
+        static_assert(operations.size() == 8);
+
+        constexpr auto alternateSum = [&]() {
+            auto sum = 0;
+            for (auto i : rv::iota(0uz, operations.size())) {
+                sum += i % 2 == 0 ? operations.at(i) : -operations.at(i);
+            }
+            return sum;
+        }();
+        static_assert(alternateSum == 46);
+
+        dsa::RootishArray<Type> list{};
+        for (auto i : rv::iota(0uz, operations.size())) {
+            for (auto j : rv::iota(0uz, (std::size_t)operations.at(i))) {
+                if (i % 2 == 0) {
+                    list.push_back(int(j + i + (std::size_t)operations.at(i) / 3uz));
+                } else {
+                    list.pop_back();
+                }
+            }
+        }
+        expect(list.size() == alternateSum);
+
+        // each block should have a capacity of i + 1 and fully filled except the last two blocks
+        for (auto size = list.blocks().size(); auto i : rv::iota(0uz, size)) {
+            expect(that % list.block(i).capacity() == i + 1);
+            if (i < size - 2) {
+                expect(that % list.block(i).size() == list.block(i).capacity());
+            }
+        }
+
+        // print once, cause I want to see the blocks, lol
+        if constexpr (std::same_as<Type, test_util::Regular>) {
+            // fmt::print("list [{}@{}]: {}\n", list.size(), list.blocks().size(), list.blocks());
+            std::string listStr{};
+            for (const auto& block : list.blocks()) {
+                listStr += fmt::format("\t{:>3}: {}\n", block.size(), block);
+            }
+            fmt::println("list [{}@{}]: {{\n{}}}", list.size(), list.blocks().size(), listStr);
+        }
+    };
+}
+
+int main()
+{
+    // main types
+    test<test_util::Regular>();
+    test<test_util::MovableOnly<>>();
+    test<test_util::CopyableOnly<>>();
+
+    // extra
+    test_util::forEachType<test_util::NonTrivialPermutations>([]<typename T>() {
+        if constexpr (dsa::ArrayElement<T>) {
+            test<T>();
+        }
+    });
+}
